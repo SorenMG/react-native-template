e07ca6af0261709fe785b84828b4cb2d9645266a
Søren Møller Gade Hansen <soren@Srens-MacBook-Pro.local>
2021-01-09 15:23:15 +0100

    add web support

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container } from '@components/index'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Container>
+        <Text>Test</Text>
+      </Container>
       <StatusBar style="auto" />
     </View>
   );
