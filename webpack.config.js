a4e9806474c178bdd7880c2a9289bd2b49c3e22b
Søren Møller Gade Hansen <soren@Srens-MacBook-Pro.local>
2021-01-09 15:28:55 +0100

    add foldes

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,13 @@ module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // If you want to add a new alias to the config.
   config.resolve.alias['@components'] = './src/components';
+  config.resolve.alias['@stores'] = './src/stores';
+  config.resolve.alias['@styles'] = './src/styles';
+  config.resolve.alias['@navigations'] = './src/navigations';
+  config.resolve.alias['@scenes'] = './src/scenes';
+  config.resolve.alias['@utils'] = './src/utils';
+  config.resolve.alias['@assets'] = './src/assets';
+  config.resolve.alias['@services'] = './src/services';
 
   // Maybe you want to turn off compression in dev mode.
   if (config.mode === 'development') {
